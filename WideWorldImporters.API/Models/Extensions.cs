aef8efe75698f8d514679394cf7ab52f144f8b9f
Lucas M <lucas.inst@outlook.com.br>
2020-10-20 21:12:37 -0300

    TP  Finalizado

---
+++ WideWorldImporters.API/Models/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WideWorldImporters.API.Models
+{
+#pragma warning disable CS1591
+    public static class WideWorldImportersDbContextExtensions
+    {
+        public static IQueryable<StockItem> GetStockItems(this WideWorldImportersDbContext dbContext, int pageSize = 10, int pageNumber = 1, int? lastEditedBy = null, int? colorID = null, int? outerPackageID = null, int? supplierID = null, int? unitPackageID = null)
+        {
+            var query = dbContext.StockItems.AsQueryable();
+
+            if (lastEditedBy.HasValue)
+            { query = query.Where(item => item.LastEditedBy == lastEditedBy); }
+
+            if (colorID.HasValue)
+            { query = query.Where(item => item.ColorID == colorID); }
+
+            if (outerPackageID.HasValue)
+            { query = query.Where(item => item.OuterPackageID == outerPackageID); }
+
+            if (supplierID.HasValue)
+            { query = query.Where(item => item.SupplierID == supplierID); }
+
+            if (unitPackageID.HasValue)
+            { query = query.Where(item => item.UnitPackageID == unitPackageID); }
+
+            return query;
+        }
+
+        public static async Task<StockItem> GetStockItemsAsync(this WideWorldImportersDbContext dbContext, StockItem entity)
+            => await dbContext.StockItems.FirstOrDefaultAsync(item => item.StockItemID == entity.StockItemID);
+
+        public static async Task<StockItem> GetStockItemsByStockItemNameAsync(this WideWorldImportersDbContext dbContext, StockItem entity)
+            => await dbContext.StockItems.FirstOrDefaultAsync(item => item.StockItemName == entity.StockItemName);
+    }
+
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<TModel> Paging<TModel>(this IQueryable<TModel> query, int pageSize = 0, int pageNumber = 0) where TModel : class
+            => pageSize > 0 && pageNumber > 0 ? query.Skip((pageNumber - 1) * pageSize).Take(pageSize) : query;
+    }
+#pragma warning restore CS1591
+}
---
+++ WideWorldImporters.API/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using WideWorldImporters.API.Controllers;
+using WideWorldImporters.API.Models;
+
+namespace WideWorldImporters.API
+{
+#pragma warning disable CS1591
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<WideWorldImportersDbContext>(builder =>
+            {
+                builder.UseSqlServer(Configuration["AppSettings:ConnectionString"]);
+            });
+
+            services.AddScoped<ILogger, Logger<WarehouseController>>();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "WideWorldImporters API", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                options.IncludeXmlComments(xmlPath);
+            });
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "WideWorldImporters API V1"); });
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseCors();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+            });
+        }
+    }
+#pragma warning restore CS1591
+}
